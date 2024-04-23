34079632141a0f654983616b65dbfbef859ee16d
shrutibharti <shrutibharti02sjps@gmail.com>
2024-04-23 22:48:36 +0530

    updating mou page and fee structure page

---
+++ mous.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MOU</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+
+</head>
+
+<body>
+    <!-- MAIN DIV FOR THE CONTENT -->
+    <!-- header start -->
+    <div>
+        <?php include '../../includes/header/header.php'; ?>
+    </div>
+    <!-- header end -->
+
+    <div class="rounded-lg  p-4">
+
+        <div class="title flex items-center bg-[#832824] rounded-lg my-5">
+
+
+            <img class="w-10 h-7 pl-3" src="../assets/icons/MOUicons.png">
+            <div class="p-2">
+                <strong class="font-extrabold   text-white text-base md:text-lg lg:text-3xl mx-3 float-left "></text>
+                    MEMORANDUM OF UNDERSTANDING</strong>
+            </div>
+
+        </div>
+        <!--content of the body for the picture -->
+
+        <div class="grid grid-flow-row gap-8 place-content-center grid-col-1  md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 justify-items-center">
+            <a href="https://nitsikkim.ac.in/documents/Project/2022/MOU%20for%20IIT%20Hyderabad%2008.07.22.pdf">
+                <div class=" flex-col gap-2 transition duration-300 ease-in-out hover:scale-110">
+                    <img src="../assets/image/mouPicture/iithyderabad.png" alt="image" class=" h-48 w-48">
+                    <p class="text-center text-2xl">IIT Hyderabad</p>
+
+                </div>
+            </a>
+            <a href="https://nitsikkim.ac.in/documents/Project/2022/MoU_IIT_Bhilai.pdf">
+                <div class=" flex-col gap-2transition duration-300 ease-in-out hover:scale-110   ">
+                    <img src="../assets/image/mouPicture/iitbhilai.png" alt="image" class="object-fill h-48 w-48">
+                    <p class="text-center text-2xl">IIT Bhilai</p>
+                </div>
+            </a>
+            <a href="https://nitsikkim.ac.in/documents/Project/2021/1635415469463_MoU-IITB-NITSikkim.pdf">
+                <div class=" flex-col gap-2transition duration-300 ease-in-out hover:scale-110 ">
+                    <img src="../assets/image/mouPicture/iitbombay.png" alt="image" class="object-fill h-48 w-48">
+                    <p class="text-center text-2xl">IIT Bombay</p>
+                </div>
+            </a>
+            <a href="https://nitsikkim.ac.in/documents/Project/2021/IITD%20NITSikkim%20MoU.pdf">
+                <div class=" flex-col gap-2 transition duration-300 ease-in-out hover:scale-110  ">
+                    <img src="../assets/image/mouPicture/iitdelhi.png" alt="image" class="object-fill h-48 w-48">
+                    <p class="text-center text-2xl">IIT Delhi</p>
+                </div>
+            </a>
+            <a href="https://nitsikkim.ac.in/documents/Project/2021/MOU%20IIT%20Gandhinagar.pdf">
+                <div class=" flex-col gap-2 transition duration-300 ease-in-out hover:scale-110  ">
+                    <img src="../assets/image/mouPicture/iitgandhinagar.png" alt="image" class="object-fill h-48 w-48">
+                    <p class="text-center text-2xl">IIT Gandhinagar</p>
+                </div>
+            </a>
+            <a href="https://nitsikkim.ac.in/documents/Project/2021/MoU_IIT_Guwahati.pdf">
+                <div class=" flex-col gap-2 transition duration-300 ease-in-out hover:scale-110 ">
+                    <img src="../assets/image/mouPicture/iitguwahati.png" alt="image" class="object-fill h-48 w-48">
+                    <p class="text-center text-2xl">IIT Guwahati</p>
+                </div>
+            </a>
+            <a href="https://nitsikkim.ac.in/documents/Project/2021/MoU_NIT%20Sikkim-IITR.pdf">
+                <div class=" flex-col gap-2 transition duration-300 ease-in-out hover:scale-110 ">
+                    <img src="../assets/image/mouPicture/iitr.png" alt="image" class="object-fill h-48 w-48">
+                    <p class="text-center text-2xl">IIT Roorkee</p>
+                </div>
+            </a>
+            <a href="https://nitsikkim.ac.in/documents/Project/2022/MOU_TCGLS.pdf">
+                <div class=" flex-col gap-2 transition duration-300 ease-in-out hover:scale-110 ">
+                    <img src="../assets/image/mouPicture/tcgLifeSciences.png" alt="image" class="object-fill h-48 w-48">
+                    <p class="text-center text-2xl">TCGLS</p>
+                </div>
+            </a>
+            <a href="https://nitsikkim.ac.in/documents/Project/2022/MoU_DGRE-DRDO.pdf">
+                <div class=" flex-col gap-2 transition duration-300 ease-in-out hover:scale-110 ">
+                    <img src="../assets/image/mouPicture/drdo.png" alt="image" class="object-fill h-48 w-48">
+                    <p class="text-center text-2xl">DRGE-DRDO</p>
+                </div>
+            </a>
+            <a href="https://nitsikkim.ac.in/documents/Project/2022/MoU_NHIDEL.pdf">
+                <div class=" flex-col gap-2 transition duration-300 ease-in-out hover:scale-110 ">
+                    <img src="../assets/image/mouPicture/nhidcl.png" alt="image" class="object-fill h-48 w-48">
+                    <p class="text-center text-2xl">NHIDCL</p>
+                </div>
+            </a>
+            <a href="https://nitsikkim.ac.in/documents/Project/2022/MoU_MNIT%20Jaipur.pdf">
+                <div class=" flex-col gap-2 transition duration-300 ease-in-out hover:scale-110 ">
+                    <img src="../assets/image/mouPicture/mnitjaipur.png" alt="image" class="object-fill h-48 w-48">
+                    <p class="text-center text-2xl">MNIT Jaipur</p>
+                </div>
+            </a>
+            <a href="https://nitsikkim.ac.in/documents/Project/2022/MoU_NIPER_Kolkata.pdf">
+                <div class=" flex-col gap-2 transition duration-300 ease-in-out hover:scale-110 ">
+                    <img src="../assets/image/mouPicture/niper.png" alt="image" class="object-fill h-48 w-48">
+                    <p class="text-center text-2xl">NIPER Kolkata</p>
+                </div>
+            </a>
+            <a href="https://nitsikkim.ac.in/documents/Project/2023/VNIT%20MOU.pdf">
+                <div class=" flex-col gap-2 transition duration-300 ease-in-out hover:scale-110">
+                    <img src="../assets/image/mouPicture/vnit.png" alt="image" class="object-fill h-48 w-48">
+                    <p class="text-center text-2xl">VNIT</p>
+                </div>
+            </a>
+            <a href="https://nitsikkim.ac.in/documents/Project/2018/MoU_CCCT_Chisopani_NIT%20Sikkim.pdf">
+                <div class=" flex-col gap-2 transition duration-300 ease-in-out hover:scale-110 ">
+                    <img src="../assets/image/mouPicture/ccct.png" alt="image" class="object-fill h-48 w-48">
+                    <p class="text-center text-2xl">CCCT Chisopani</p>
+                </div>
+            </a>
+            <a href="https://nitsikkim.ac.in/documents/Project/2023/BIS%20MOU.pdf">
+                <div class=" flex-col gap-2 transition duration-300 ease-in-out hover:scale-110 ">
+                    <img src="../assets/image/mouPicture/bueroofindianstandard.png" alt="image" class="object-fill h-65 w-65">
+                    <p class="text-center text-2xl">Bureau of Indian Standards</p>
+                </div>
+            </a>
+        </div>
+        <!--last content -->
+        <div class="flex flex-wrap mx-auto  my-4">
+            <div class="content p-5">
+                <!--- heading-->
+                <hr class=" bg-black">
+                <div class="text-center text-red-500 font-bold my-4">
+                    TECHNICAL UNIVERSITY GERMANY
+
+                </div>
+
+                <div class=" text-center font-semibold p-2 text-sm lg:text-base text-justify" >
+                    <P>National Institute of Technology Sikkim has signed the <span class="font-bold">ABROAD LEARNING AGREEMENT</span> with Centre for
+                        Information Services and High Performance Computing (ZIH), TU Germany. The agreement
+                        materialised when
+                        one of our students, Mr. Ankur Sharma (Roll. B100005CS ) joined the Centre for Information
+                        Services and
+                        High Performance Computing (ZIH), TU Dresden Germany as a visiting exchange student to conduct
+                        his one
+                        semester along with simultaneous project work for his Bachelor thesis at CIHP, TU Dresden,
+                        Germany.
+                    </P>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+    <div>
+
+        <!-- footer start -->
+        <?php include '../../includes/footer.php'; ?>
+        <!-- footer end -->
+
+    </div>
+</body>
+
+</html>
